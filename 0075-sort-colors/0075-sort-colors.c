46e83c0e01fd950fc754dcb4d1f50db61b6b284f
Ashish Shaw <x8201257@gmail.com>
2026-08-23 15:10:41 +0530

    Time: 3 ms (1.86%), Space: 10.5 MB (40.27%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.c
@@ -0,0 +1,11 @@
+void sortColors(int* nums, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (nums[j] > nums[j + 1]) {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+    }
+}
