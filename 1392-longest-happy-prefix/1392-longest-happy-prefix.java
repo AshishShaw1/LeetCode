60bd18d644025b40f0905e85746fa97e3c2df5ae
Ashish Shaw <x8201257@gmail.com>
2026-08-31 21:03:25 +0530

    Time: 6 ms (99.61%), Space: 47.9 MB (24.35%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestPrefix(String s) {
+        char[] arr = s.toCharArray();
+        int[] lps = new int[s.length()];
+        int i = 0;
+        int j = 1; 
+        while (j < arr.length) {
+            if (arr[i] == arr[j]) {
+                lps[j] = ++i;
+                j++;
+            } else {
+                if (i == 0) {
+                    lps[j++] = 0;
+                } else {
+                    i = lps[i - 1];
+                }
+            }
+        }
+        if (lps[arr.length - 1] == 0)
+            return "";
+        int start = arr.length - lps[arr.length - 1];
+        return s.substring(start, arr.length);
+    }
+}
